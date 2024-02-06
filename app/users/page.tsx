42787ed6af5994d14ac1791a8ff8e24b30d915d8
yumura <yumura.s@tsunagu-grp.jp>
2024-02-06 12:00:22 +0900

    first commit

---
+++ app/users/page.tsx
@@ -0,0 +1,16 @@
+const Page = async() => {
+  await new Promise((resolve) => setTimeout(resolve, 5000));
+  const response = await fetch('https://jsonplaceholder.typicode.com/users')
+  const users = await response.json()
+  return(
+    <div className="m-4">
+      <h1 className="text-lg font-bold">ユーザ一覧</h1>
+      <ul>
+        {users.map((user:any) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
+    </div>
+  )
+}
+export default Page;
